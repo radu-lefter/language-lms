89843a69437ff9e93d0cf231cb735fb441f3297d
Radu Lefter <lefter_radu@yahoo.com>
2024-02-21 18:17:12 +0000

    created admin logout route and method

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
@@ -10,5 +11,20 @@ class AdminController extends Controller
 
         return view('admin.index');
 
+    } // End Method 
+
+    public function AdminLogout(Request $request) {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        $notification = array(
+            'message' => 'Logout Successfully',
+            'alert-type' => 'info'
+        );
+ 
+        return redirect('/admin/login')->with($notification);
     } // End Method 
 }
