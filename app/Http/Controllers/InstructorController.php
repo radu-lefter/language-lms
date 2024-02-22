6ad992f89463087575888f82e9f30fe2c7d61807
Radu Lefter <lefter_radu@yahoo.com>
2024-02-22 18:00:21 +0000

    setup basic template for instructor

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class InstructorController extends Controller
 {
     public function InstructorDashboard(){
-        return view('instructor.instructor_dashboard');
+        return view('instructor.index');
     } // End Mehtod 
+
 }
