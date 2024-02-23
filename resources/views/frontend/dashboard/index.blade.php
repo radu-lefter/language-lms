ea6677c391e03c0b50ee55a063d678d68111f64c
Radu Lefter <lefter_radu@yahoo.com>
2024-02-23 18:51:12 +0000

    displayed user name and photo on user dashboard

--- resources/views/frontend/dashboard/index.blade.php
+++ resources/views/frontend/dashboard/index.blade.php
@@ -1,18 +1,18 @@
 @extends('frontend.dashboard.user_dashboard')
 @section('userdashboard')
 
-{{-- @php
+@php
     $id = Auth::user()->id;
     $profileData = App\Models\User::find($id);
-@endphp --}}
+@endphp
 
 <div class="breadcrumb-content d-flex flex-wrap align-items-center justify-content-between mb-5">
     <div class="media media-card align-items-center">
-        {{-- <div class="media-img media--img media-img-md rounded-full">
+        <div class="media-img media--img media-img-md rounded-full">
             <img class="rounded-full" src="{{ (!empty($profileData->photo)) ? url('upload/user_images/'.$profileData->photo) : url('upload/no_image.jpg')}}" alt="Student thumbnail image">
-        </div> --}}
+        </div>
         <div class="media-body">
-            {{-- <h2 class="section__title fs-30">Hello, {{ $profileData->name }}</h2> --}}
+            <h2 class="section__title fs-30">Hello, {{ $profileData->name }}</h2>
             <div class="rating-wrap d-flex align-items-center pt-2">
                 <div class="review-stars">
                     <span class="rating-number">4.4</span>
