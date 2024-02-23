8e48ad9cd511800626336240733ddb61a6592a82
Radu Lefter <lefter_radu@yahoo.com>
2024-02-23 19:22:59 +0000

    implemented logout and change password functionality for user

--- resources/views/frontend/dashboard/edit_profile.blade.php
+++ resources/views/frontend/dashboard/edit_profile.blade.php
@@ -19,8 +19,7 @@
     <div class="setting-body">
         <h3 class="fs-17 font-weight-semi-bold pb-4">Edit Profile</h3>
        
-        {{-- <form method="post" action="{{ route('user.profile.update') }}" enctype="multipart/form-data" class="row pt-40px"> --}}
-        <form method="post" action="" enctype="multipart/form-data" class="row pt-40px">
+        <form method="post" action="{{ route('user.profile.update') }}" enctype="multipart/form-data" class="row pt-40px">
         @csrf
        
         <div class="media media-card align-items-center">
