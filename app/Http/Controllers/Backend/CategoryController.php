0707ba8ff1f736be254a17222ad104786cb8bf50
Radu Lefter <lefter_radu@yahoo.com>
2024-03-04 16:28:01 +0000

    created model and controller for category functionality

---
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoryController extends Controller
+{
+    public function AllCategory(){
+
+        
+
+    }// End Method
+}
